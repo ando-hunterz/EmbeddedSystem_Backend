a706b87b4c6b3364ba738e86295f63c11d08d02b
LENOVO <minovfalk@gmail.com>
2021-05-06 03:12:09 +0700

    added few fixes

--- middleware/jwtHandler.js
+++ middleware/jwtHandler.js
@@ -3,15 +3,15 @@ const jwt = require("jsonwebtoken");
 module.exports = function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null)
+  if (token === null)
     return res
       .sendStatus(401)
-      .send({ messages: ["No Jwt Token Supplied"], fields: ["jwtToken"] }); // if there isn't any token
+      .json({ messages: ["No Jwt Token Supplied"], fields: ["jwtToken"] }); // if there isn't any token
   jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, user) => {
     if (err)
       return res
         .sendStatus(403)
-        .send({ messages: ["JwtToken Expired!"], fields: ["jwtToken"] });
+        .json({ messages: ["JwtToken Expired!"], fields: ["jwtToken"] });
     next(); // pass the execution off to whatever request the client intended
   });
 };
